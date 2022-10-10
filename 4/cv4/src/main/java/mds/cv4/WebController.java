638259d3f83ddab9269d0e123ffe6671675ef563
Mefitek <mathew2000@email.cz>
2022-10-10 09:53:57 +0200

    aaaaa

--- 4/cv4/src/main/java/mds/cv4/WebController.java
+++ 4/cv4/src/main/java/mds/cv4/WebController.java
@@ -1,3 +1,22 @@
+/*
+V třídě WebController vytvořte 3 metody, které budou splňovat následující:
+
+•Metoda myself bude naslouchat na adrese myself . Bude použita statická šablona, kde
+bude napsáno vaše jméno a id. Obojí bude v elementu <h1> a jméno bude červeně a
+ID tučně v kombinací s jinou barvou dle vaší volby.
+
+• Další metody budou alice a bob , každá bude naslouchat na jiné alice , bob
+
+•Obě metody budou využívat jednu dynamickou šablonu (template.html) a 2 rozdílné
+css soubory, se jmény odpovídající metodám (tzn. a lice.css a bob.css)
+
+•Alice bude napsána jako <h1>, kurzívou a červeně
+
+• Bob bude taktéž v <h1>, podtrženě a modrou barvou
+
+• Css lze dynamicky měnit: th:href css /' + name } + css
+ */
+
 package mds.cv4;
 
 public class WebController {
